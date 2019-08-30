c9066fb398d402bfbe67137ef6652a23538cced2
fuhl <814073395@qq.com>
2019-08-30 15:30:53 +0800

    更换域名

--- js/hm.js
+++ js/hm.js
@@ -1,7 +1,7 @@
 // (function () {
 //     var h = {}, mt = {}, c = {
 //         id: "5fa4f2fc5624f544affb211eab296f46",
-//         dm: ["xfigo.com", "m-xfigo-com-2aa4.aipage.cn", "wap.qudengshan.cn", "qudengshan.cn"],
+//         dm: ["cchtw.com", "m-cchtw-com-2aa4.aipage.cn", "wap.qudengshan.cn", "qudengshan.cn"],
 //         js: "tongji.baidu.com/hm-wap/js/",
 //         etrk: [],
 //         cetrk: [],
